ba7a3360db6bbebeade7ffd13b66dcf5d0cd7c16
Paul Lee <ctwdtw@gmail.com>
2023-02-03 17:19:22 +0800

    fix typo on `frequentRenterPoints`

--- Sources/refactoring_java_ed/Customer.swift
+++ Sources/refactoring_java_ed/Customer.swift
@@ -20,12 +20,12 @@ public class Customer {
     
     public func statement() -> String {
         var totalAmount: Double  = 0
-        var frequentRentalPoints: Int = 0
+        var frequentRenterPoints: Int = 0
         var result: String = "Rental Record for \(name)\n"
         // determine amounts for each rental
         rentals.forEach { each in
             // add frequent renter points
-            frequentRentalPoints += each.getFrequentRenterPoints()
+            frequentRenterPoints += each.getFrequentRenterPoints()
             // show figures for this rental
             result += "  \(each.movie.title)  \(each.getCharge())\n"
             totalAmount += each.getCharge()
@@ -33,7 +33,7 @@ public class Customer {
         
         // add footer lines
         result += "Amount owed is \(totalAmount)\n"
-        result += "You earned \(frequentRentalPoints) frequent renter points"
+        result += "You earned \(frequentRenterPoints) frequent renter points"
         return result
     }
 }
