c743c22406ff565bf2f4450e52e0c9a30acfb442
Paul Lee <ctwdtw@gmail.com>
2023-02-03 17:33:58 +0800

    Apply `Replace Temp with Query (120)` on `totalAmount`

--- Sources/refactoring_java_ed/Customer.swift
+++ Sources/refactoring_java_ed/Customer.swift
@@ -19,7 +19,6 @@ public class Customer {
     }
     
     public func statement() -> String {
-        var totalAmount: Double  = 0
         var frequentRenterPoints: Int = 0
         var result: String = "Rental Record for \(name)\n"
         // determine amounts for each rental
@@ -28,12 +27,19 @@ public class Customer {
             frequentRenterPoints += each.getFrequentRenterPoints()
             // show figures for this rental
             result += "  \(each.movie.title)  \(each.getCharge())\n"
-            totalAmount += each.getCharge()
         }
         
         // add footer lines
-        result += "Amount owed is \(totalAmount)\n"
+        result += "Amount owed is \(getTotalCharge())\n"
         result += "You earned \(frequentRenterPoints) frequent renter points"
         return result
+    }
+    
+    private func getTotalCharge() -> Double {
+        var totalAmount: Double  = 0
+        rentals.forEach { each in
+            totalAmount += each.getCharge()
+        }
+        return totalAmount
     }
 }
