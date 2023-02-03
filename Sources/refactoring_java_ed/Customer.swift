02d045f7e26b70e1c5d90c35eb60a2c1d8d5f399
Paul Lee <ctwdtw@gmail.com>
2023-02-03 17:36:37 +0800

    Apply `Replace Temp with Query (120)` on `frequentRenterPoints`

--- Sources/refactoring_java_ed/Customer.swift
+++ Sources/refactoring_java_ed/Customer.swift
@@ -19,19 +19,16 @@ public class Customer {
     }
     
     public func statement() -> String {
-        var frequentRenterPoints: Int = 0
         var result: String = "Rental Record for \(name)\n"
         // determine amounts for each rental
         rentals.forEach { each in
-            // add frequent renter points
-            frequentRenterPoints += each.getFrequentRenterPoints()
             // show figures for this rental
             result += "  \(each.movie.title)  \(each.getCharge())\n"
         }
         
         // add footer lines
         result += "Amount owed is \(getTotalCharge())\n"
-        result += "You earned \(frequentRenterPoints) frequent renter points"
+        result += "You earned \(getTotalFrequentRenterPoints()) frequent renter points"
         return result
     }
     
@@ -41,5 +38,13 @@ public class Customer {
             totalAmount += each.getCharge()
         }
         return totalAmount
+    }
+    
+    private func getTotalFrequentRenterPoints() -> Int {
+        var frequentRenterPoints: Int = 0
+        rentals.forEach { each in
+            frequentRenterPoints += each.getFrequentRenterPoints()
+        }
+        return frequentRenterPoints
     }
 }
