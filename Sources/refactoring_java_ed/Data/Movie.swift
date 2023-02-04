5ef96560862c1a8725d1b90ea9845607aefaa9f8
Paul Lee <ctwdtw@gmail.com>
2023-02-04 19:55:33 +0800

    Apply `Move Method (142)` to `Movie.getCharge(from:)` and `Movie.getFrequentRenterPoints(from:)` as a pre-step for `Replace Conditional with Polymorphism (110)`

--- Sources/refactoring_java_ed/Data/Movie.swift
+++ Sources/refactoring_java_ed/Data/Movie.swift
@@ -57,31 +57,10 @@ public class Movie {
     }
     
     func getCharge(from daysRented: Int) -> Double {
-        var thisAmount: Double = 0
-        switch getPriceCode() {
-        case .regular:
-            thisAmount += 2
-            if daysRented > 2 {
-                thisAmount += Double(daysRented - 2)*1.5
-            }
-        case .newRelease:
-            thisAmount += Double(daysRented * 3)
-        case .childrens:
-            thisAmount += 1.5
-            if daysRented > 3 {
-                thisAmount += Double(daysRented - 3)*1.5
-            }
-        }
-        
-        return thisAmount
+        price.getCharge(from: daysRented)
     }
     
     func getFrequentRenterPoints(from daysRented: Int) -> Int {
-        var result = 1
-        // add bonus for a two day new release rental
-        if getPriceCode() == .newRelease && daysRented > 1 {
-            result += 1
-        }
-        return result
+        price.getFrequentRenterPoints(from: daysRented)
     }
 }
